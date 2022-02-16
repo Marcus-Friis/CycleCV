f042ff40762378214d634bfa4c5cafd53adcb1af
Marcus Friis <mahf@itu.dk>
2022-02-16 16:03:22 +0100

    pytorch virk?

--- pytorch.py
+++ pytorch.py
@@ -5,6 +5,10 @@ import pandas as pd
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
+import datetime
+from datetime import date
+import matplotlib.pyplot as plt
+
 
 class RNNModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
@@ -35,6 +39,7 @@ class RNNModel(nn.Module):
         # Convert the final state to our desired output shape (batch_size, output_dim)
         out = self.fc(out)
         return out
+
 
 class LSTMModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
@@ -73,6 +78,7 @@ class LSTMModel(nn.Module):
 
         return out
 
+
 class GRUModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
         super(GRUModel, self).__init__()
@@ -104,6 +110,8 @@ class GRUModel(nn.Module):
         out = self.fc(out)
 
         return out
+
+
 def get_model(model, model_params):
     models = {
         "rnn": RNNModel,
@@ -114,19 +122,20 @@ def get_model(model, model_params):
 
 
 class Optimization:
-    def __init__(self, model, loss_fn, optimizer):
+    def __init__(self, model, loss_fn, optimizer, device='cpu'):
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.train_losses = []
         self.val_losses = []
+        self.device = device
     
     def train_step(self, x, y):
         # Sets model to train mode
         self.model.train()
 
         # Makes predictions
-        yhat = self.model(x)
+        yhat = self.model(x).to(self.device)
 
         # Computes loss
         loss = self.loss_fn(y, yhat)
@@ -141,20 +150,56 @@ class Optimization:
         # Returns the loss
         return loss.item()
     
-    def train(self, train_loader, batch_size=64, n_epochs=50, n_features=1):
+    def train(self, train_loader, val_loader, batch_size=64, n_epochs=50, n_features=1):
+        model_path = f'models/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
 
         for epoch in range(1, n_epochs + 1):
             batch_losses = []
             for x_batch, y_batch in train_loader:
-                x_batch = x_batch.view([batch_size, -1, n_features])#.to(device)
-                y_batch = y_batch#.to(device)
+                x_batch = x_batch.view([batch_size, -1, n_features]).to(self.device)
+                y_batch = y_batch.to(self.device)
                 loss = self.train_step(x_batch, y_batch)
                 batch_losses.append(loss)
             training_loss = np.mean(batch_losses)
             self.train_losses.append(training_loss)
 
+            with torch.no_grad():
+                batch_val_losses = []
+                for x_val, y_val in val_loader:
+                    x_val = x_val.view([batch_size, -1, n_features]).to(self.device)
+                    y_val = y_val.to(self.device)
+                    self.model.eval()
+                    yhat = self.model(x_val)
+                    val_loss = self.loss_fn(y_val, yhat).item()
+                    batch_val_losses.append(val_loss)
+                validation_loss = np.mean(batch_val_losses)
+                self.val_losses.append(validation_loss)
+
+            if (epoch <= 10) | (epoch % 50 == 0):
                 print(
-                    f"[{epoch}/{n_epochs}] Training loss: {training_loss:.4f}\t"
+                    f"[{epoch}/{n_epochs}] Training loss: {training_loss:.4f}\t Validation loss: {validation_loss:.4f}"
                 )
 
         torch.save(self.model.state_dict(), model_path)
+
+    def evaluate(self, test_loader, batch_size=1, n_features=1):
+        with torch.no_grad():
+            predictions = []
+            values = []
+            for x_test, y_test in test_loader:
+                x_test = x_test.view([batch_size, -1, n_features]).to(self.device)
+                y_test = y_test.to(self.device)
+                self.model.eval()
+                yhat = self.model(x_test)
+                predictions.append(yhat.to(self.device).detach().numpy())
+                values.append(y_test.to(self.device).detach().numpy())
+
+        return predictions, values
+
+    def plot_losses(self):
+        plt.plot(self.train_losses, label="Training loss")
+        plt.plot(self.val_losses, label="Validation loss")
+        plt.legend()
+        plt.title("Losses")
+        plt.show()
+        plt.close()
