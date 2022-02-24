5bd3bea109d072ab63e39bcd7e4bdd23c8096d9d
Marcus Friis <mahf@itu.dk>
2022-02-24 12:43:02 +0100

    maybe padding works?

--- virkpls.py
+++ virkpls.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
-from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 
 import datetime
 import pickle
 import numpy as np
+
 
 class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
@@ -23,28 +24,29 @@ class LSTM(nn.Module):
         self.fc = nn.Linear(self.hidden_dim, self.output_dim)
 
     # TODO: rewrite (hn, cn) as hiddenstate
-    def forward(self, x, hn=None, cn=None):
-        if hn is not None or cn is not None:
-            output, (hn, cn) = self.lstm(x, (hn, cn))
-        else:
-            output, (hn, cn) = self.lstm(x)
-        output = self.fc(output)
+    def forward(self, x):
+        lengths = x.shape[1] - torch.sum(x[:, :, -1] == torch.tensor(0), axis=1)
+        x_pack = pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
 
-        return output, (hn, cn)
+        output_pack, hidden = self.lstm(x_pack)
+        x_unpack, lens_unpack = pad_packed_sequence(output_pack, batch_first=True)
+        output = self.fc(x_unpack)
+
+        return output, hidden
 
     # TODO: improve predicting method
-    def mikkel(self, x, future_preds=10):
-        out = x
-        output = out[:, -1:]
-        batch = output.shape[0]
-        cn = torch.zeros(2, batch, 64)
-        hn = torch.zeros(2, batch, 64)
-        for i in range(future_preds):
-            out, (hn, cn) = self.forward(out, hn=hn, cn=cn)
-            out = out[:, -1:, :]
-            out = torch.concat((out, output[:, -1:, :2], output[:, -1:, 4:]), axis=2)
-            output = torch.concat((output, out), axis=1)
-        return output
+    # def mikkel(self, x, future_preds=10):
+    #     out = x
+    #     output = out[:, -1:]
+    #     batch = output.shape[0]
+    #     cn = torch.zeros(2, batch, 64)
+    #     hn = torch.zeros(2, batch, 64)
+    #     for i in range(future_preds):
+    #         out, (hn, cn) = self.forward(out, hn=hn, cn=cn)
+    #         out = out[:, -1:, :]
+    #         out = torch.concat((out, output[:, -1:, :2], output[:, -1:, 4:]), axis=2)
+    #         output = torch.concat((output, out), axis=1)
+    #     return output
 
 
 if __name__ == '__main__':
@@ -52,8 +54,6 @@ if __name__ == '__main__':
     net = LSTM(6, 64, 2, 2)  # (input_size, hidden_size, output_size, num_layers)
     x = torch.randn(4, 3, 6)  # batch_first (batch, time_steps, input_size) else (times_steps, batch, input_size)
     out, (hn, cn) = net(x)
-
-    asd = net.mikkel(x)
 
     # TODO: normalize data before pickling
     with open('jeff.pkl', 'rb') as f:
@@ -88,17 +88,15 @@ if __name__ == '__main__':
             targets.append(target)
 
     # TODO: implement packed paddings to use full sequences
-    # inputs = pad_sequence(inputs, batch_first=True)
+    inputs = pad_sequence(inputs, batch_first=True)
     # inputs = pack_padded_sequence(inputs, lengths, batch_first=True, enforce_sorted=False)
-    #
-    # targets = pad_sequence(targets, batch_first=True)
-    # targets = pack_padded_sequence(targets, lengths, batch_first=True, enforce_sorted=False)
 
-    model = LSTM(6, 64, 2, 2)
+    targets = pad_sequence(targets, batch_first=True)
+    # targets = pack_padded_sequence(targets, lengths, batch_first=True, enforce_sorted=False)
 
     batch_size = 64
     dropout = 0.2
-    n_epochs = 200
+    n_epochs = 50
     learning_rate = 1e-3
     weight_decay = 1e-6
 
@@ -117,6 +115,7 @@ if __name__ == '__main__':
         for x_batch, y_batch in train_loader:
             optimizer.zero_grad()  # zero the gradient buffers
             output, (hn, cn) = model(x_batch)
+            y_batch = y_batch[:,:output.shape[1]]
             loss = loss_fn(output, y_batch)
             loss.backward()
             optimizer.step()
@@ -144,9 +143,9 @@ if __name__ == '__main__':
     for n in plot_this:
         ax.scatter(n[:,0]*1280, n[:,1]*720, c='b')
 
-    out = model.mikkel(plot_this, 10)
-    for n in out.detach():
-        ax.scatter(n[:,0]*1280, n[:,1]*720, c='r')
+    # out = model.mikkel(plot_this, 10)
+    # for n in out.detach():
+    #     ax.scatter(n[:,0]*1280, n[:,1]*720, c='r')
 
     print(out)
     plt.show()
