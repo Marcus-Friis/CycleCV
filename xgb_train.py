15d73720a9b7110f46d42d763531f54c34b2ce21
Marcus Friis <mahf@itu.dk>
2022-04-18 13:29:41 +0200

    added commetns

--- xgb_train.py
+++ xgb_train.py
@@ -5,19 +5,27 @@ from sklearn.model_selection import train_test_split
 from wrangler import Wrangler
 
 if __name__ == '__main__':
+    # load data
     nndf = Wrangler.load_pickle('data/nndf.pkl')
+    # get cols for training
     cols = ['x', 'y', 'd_t-1', 'd_t-2', 'd_t-3', 'd_light', 'l0', 'l1',
             'l2', 'l3', 'dir_0', 'dir_1', 'dir_2'] + ['d_zone_' + str(i) for i in range(20)]
+    # init x and y
     x = nndf[cols].to_numpy()
     y = nndf['target'].to_numpy()
 
+    # do train val test split
     x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2)
 
+    # create and fit xgb regressor
     reg = xgb.XGBModel()
     reg.fit(x_train, y_train, eval_set=[(x_val, y_val)], eval_metric='rmse', verbose=True)
 
+    # test r2_score on test set
     pred = reg.predict(x_test)
     score = r2_score(y_test, pred)
     print(score)
+
+    # save model
     Wrangler.dump_pickle(reg, 'models/xgb.pkl')
